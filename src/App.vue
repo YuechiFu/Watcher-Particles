035969e773b853d11986a27c096f6054f7a564cc
fuyuechi <609041063@qq.com>
2024-09-29 14:02:34 +0800

    build: layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
   <div>
     <div class="sticky top-0 left-0 font-mst z-10 w-full shrink-0 bg-zinc-950">
-      <div class="max768:flex-wrap py-10 flex justify-between">
-        <div
-          class="h-auto flex-wrap flex items-center justify-ceneter w-full rounded-md"
-        >
-          <div
-            v-for="item of routes[0].children"
-            class="px-8 min-h-35 mx-10 max768:mb-10 rounded-md  text-16 flex items-center"
-            :class="{ ' text-sky-300 bg-sky-950': route.name === item.name }"
+      <div
+        class="max1080:flex-wrap py-10 flex justify-between px-10 relative max1080:pr-45 mb-10"
+      >
+        <div class="hidden max1080:flex items-center absolute top-3 right-2">
+          <t-button
+            class="t-demo-collapse-btn"
+            variant="text"
+            shape="circle"
+            @click="collapsed = !collapsed"
           >
-            <router-link :to="item.path">{{ item.name }}</router-link>
-          </div>
+            <template #icon
+              ><t-icon :name="collapsed ? 'view-list' : 'close'" size="22px"
+            /></template>
+          </t-button>
         </div>
-        <div class="w-350 px-20 box-border flex items-center pr-10">
-          <t-input clearable class="w-200" v-model="wsAddr" @change="handleWs(false)"></t-input>
-          <div class="ml-10 w-100">
+        <div
+          class="h-auto flex-wrap flex items-center justify-ceneter grow rounded-md"
+          v-show="!collapsed"
+        >
+          <router-link
+            v-for="item of routes[0].children"
+            class="px-8 min-h-35 mx-10 max1080:mb-10 max1080:w-full rounded-md text-16 flex items-center"
+            :class="{ ' text-lime-300 bg-lime-950': route.name === item.name }"
+            :to="item.path"
+          >
+            {{ item.name }}
+          </router-link>
+        </div>
+        <div
+          class="focus-within:border-active grow max-w-600 overflow-hidden pl-10 box-border flex items-center pr-10 border border-zinc-600 rounded-full"
+        >
+          <input
+            clearable
+            class="block w-full outline-none border-none px-10 h-40 bg-transparent"
+            v-model="wsAddr"
+            placeholder="WebSocket URL"
+            @change="handleWs(false)"
+          />
+          <div class="flex items-center">
             <t-switch v-model="isConnected" @change="handleWs" />
           </div>
         </div>
       </div>
     </div>
-    <router-view></router-view>
+
+    <router-view class="p-10"></router-view>
+
     <TerminalBtn></TerminalBtn>
   </div>
 </template>
@@ -34,6 +60,7 @@ import { useThemeStore, useMsgStore } from "@/store";
 const route = useRoute();
 useThemeStore().setThemeMode("dark");
 const msgStore = useMsgStore();
+const collapsed = ref(false);
 let currentWs = null;
 let isConnected = ref(false);
 
@@ -44,9 +71,8 @@ const options = [
   "http://10.10.43.42:10887"
 ];
 const wsAddr = ref(
-  msgStore.ws || (location.hostname === "moon.oupp.site" ? '' : options[1])
+  msgStore.ws || (location.hostname === "moon.oupp.site" ? "" : options[1])
 );
-
 
 // https://moon.oupp.site/d2ebd48d-31e7-45e2-9414-d5714d33c999
 
@@ -54,8 +80,8 @@ const initWs = (addr) => {
   const ws = new WebSocket(addr);
   ws.addEventListener("open", () => {
     isConnected.value = true;
-    console.log(wsAddr.value)
-    msgStore.setHistoryWs(wsAddr.value)
+    console.log(wsAddr.value);
+    msgStore.setHistoryWs(wsAddr.value);
   });
   ws.addEventListener("error", (e) => {
     isConnected.value = false;
@@ -85,7 +111,7 @@ const handleWs = (open) => {
     currentWs.close();
     return;
   }
-  if(wsAddr.value){
+  if (wsAddr.value) {
     currentWs = initWs(wsAddr.value);
   }
 };
