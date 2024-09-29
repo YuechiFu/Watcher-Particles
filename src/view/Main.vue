7e073d2a213c4b8e4e206189f5b6cac737a0a1e5
fuyuechi <609041063@qq.com>
2024-09-29 13:02:13 +0800

    build: Hands Disappear. Camera will be closed

--- src/view/Main.vue
+++ src/view/Main.vue
@@ -22,10 +22,19 @@
         <t-option :value="1" label="Model">Model</t-option>
       </t-select> -->
     </div>
-    <div class="flex items-center justify-center text-active" >
-      <span class="mr-1">Handpose</span>
-      <t-switch v-model="isDetecting" ></t-switch>
-      <div id="handpose_camera_status" class="relative w-15 h-15 ml-10 flex items-center text-active"></div>
+
+    <div>
+      <div class="w-full fixed right-0 bottom-4 flex justify-center">
+        <t-alert v-if="tip">{{ tip }}</t-alert>
+      </div>
+      <div class="flex flex-wrap items-center justify-center text-active">
+        <span class="mr-1">Handpose</span>
+        <t-switch v-model="isDetecting"></t-switch>
+        <div
+          id="handpose_camera_status"
+          class="relative w-15 h-15 ml-10 flex items-center text-active"
+        ></div>
+      </div>
     </div>
 
     <div>
@@ -35,11 +44,11 @@
         :is-detecting="isDetecting"
         @update-pose="handleUpdateHandpose"
       >
-        <template
-          v-slot:default="{ isLoading, isDetecting, isCameraReady }"
-        >
+        <template v-slot:default="{ isLoading, isDetecting, isCameraReady }">
           <teleport to="#handpose_camera_status" v-if="isDetecting">
-            <div class="text-white" v-if="isLoading"><t-loading inherit-color size="19px"></t-loading></div> 
+            <div class="text-white" v-if="isLoading">
+              <t-loading inherit-color size="19px"></t-loading>
+            </div>
             <div
               class="w-15 h-15 rounded-full video_icon border-active border relative"
               v-if="isCameraReady"
@@ -54,17 +63,31 @@
 import { ref, watch } from "vue";
 import ModelParticles from "@/view/ModelParticles.vue";
 import HandPose from "@/components/HandPose";
+import { HANDPOST_TYPES } from "@/defines";
 import { useMsgStore } from "@/store";
 const currentModelKey = ref("watcher");
 const displayMode = ref(0);
 const msgStore = useMsgStore();
 const isDetecting = ref(false);
 const controls = ref(null);
+const tip = ref("");
 const changeModel = (val) => {
   currentModelKey.value = val;
 };
-
+let closeDelayTT;
 const handleUpdateHandpose = (val) => {
+  if (val.status === HANDPOST_TYPES.Disappear) {
+    console.log("Closed after 5s");
+    closeDelayTT = setTimeout(() => {
+      tip.value = "Hands Disappear. Camera will be closed after 3s";
+      setTimeout(() => {
+        isDetecting.value = false;
+      }, 3000);
+    }, 2000);
+  } else {
+    closeDelayTT && clearTimeout(closeDelayTT);
+    tip.value = "";
+  }
   if (!val.value) {
     controls.value = {
       value: ""
@@ -79,11 +102,9 @@ watch(
   (val) => {
     if (val.text) {
       changeModel(val.text);
-      isDetecting.value = !isDetecting.value;
-      
-    /*   if (val.text === "watcher") {
-        isDetecting.value = !isDetecting.value;
-      } */
+      if (!isDetecting.value) {
+        isDetecting.value = true;
+      }
     }
   }
 );
@@ -98,10 +119,10 @@ watch(
   }
 }
 .video_icon {
-  &:after{
+  &:after {
     content: "";
     position: absolute;
-    top:3px;
+    top: 3px;
     left: 3px;
     width: calc(100% - 6px);
     height: calc(100% - 6px);
