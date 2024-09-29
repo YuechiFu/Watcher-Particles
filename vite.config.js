3012b2de1fd95145a6b85e81eb3687c4589582d4
fuyuechi <609041063@qq.com>
2024-09-29 16:04:02 +0800

    feat

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import { defineConfig, splitVendorChunkPlugin } from "vite"
 import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'.',
   plugins: [
     // basicSsl(),
     vue(),
